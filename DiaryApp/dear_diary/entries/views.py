a81b850c9e240d5105c965cfe57e3c12611156e0
Pravat <44766340+Datasciencepython@users.noreply.github.com>
2018-11-10 19:25:56 +0530

    Add files via upload

---
+++ DiaryApp/dear_diary/entries/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Entry
+from .forms import EntryForm
+def index(request):
+    entries=Entry.objects.all()
+    context = {'entries': entries}
+    return render(request, 'entries/index.html',context)
+
+def add(request):
+    form=EntryForm()
+    context = {form:form}
+
+    return render(request, 'entries/add.html',context)
+
